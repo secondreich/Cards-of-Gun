be9fc167be5c49ec7ddd16db34fd7bbf7a8ca2f7
YUKAI\R <1332480703@qq.com>
2025-09-28 11:23:20 +0800

    更新AI逻辑与卡牌翻面动画

--- Gd script/main.gd
+++ Gd script/main.gd
@@ -5,29 +5,26 @@ class_name main
 @export var scene_1 : Node
 @export var scene_2 : Node
 
-@export var MaxHandCard = 7
-
-@export var siteItems : Dictionary
-
 #定义血量和上限
 @export var playerMaxLife = 3 
 @export var playerLife = 3
-
 @export var oppoentrMaxLife = 3 
 @export var oppoentLife = 3
 
-
 @export var playerMaxAmmo = 3
 @export var playerAmmo = 0
-
 @export var oppoentMaxAmmo = 3
 @export var oppoentAmmo = 0
 
+#当前使用的卡，检查复数打出机制
 var now_used_card 
 
+@export var playerDeck: Array = []
+var discard_deck: Array = []
+
 func _ready() -> void:
-	$HandCardPer.max_value = MaxHandCard
-	$PlayerProperty/PlayerHandCard.text = "手牌：     " + str(scene_1.card_total_num) + " / " + str(MaxHandCard)
+	$HandCardPer.max_value = 7
+	$PlayerProperty/PlayerHandCard.text = "手牌：     " + str(scene_1.card_total_num) + " / " + str(7)
 	$PlayerProperty/PlayerLife.text = "生命：     " + str(playerLife) + " / " + str(playerMaxLife)
 	$PlayerProperty/PlayerAmmo.text = "弹匣:       " + str(playerAmmo)+ " / " + str(playerMaxAmmo)
 	print("ready")
@@ -39,10 +36,10 @@ func _process(delta: float) -> void:
 	var handsCards = $HandCards.cardDeck.get_children()
 	
 	#判断是否下一轮
-	if scene_1.card_total_num < playerLife:
-		$DrawCard/Button.disabled = false
-	else:
-		$DrawCard/Button.disabled = true
+	#if scene_1.card_total_num < playerLife:
+		#$DrawCard/Button.disabled = false
+	#else:
+		#$DrawCard/Button.disabled = true
 	
 	#检查复数牌的打出机制
 	if not usedCards.is_empty():  
@@ -76,61 +73,76 @@ func add_new_hand_card(cardName, cardDeck , _caller = scene_1,  _caller2 = scene
 
 func get_cards():
 	var num_cards = 3
-	var total_weight = get_total_weight(siteItems)
+	var cards_drawn = 0
 	var selected_cards = []
-	for i in range(num_cards):
-		if(scene_1.card_total_num < MaxHandCard):
-			var random_num = randi() % total_weight
-			var cumulative_weight = 0
-			for c in siteItems.keys():
-				cumulative_weight += siteItems[c]
-				if random_num < cumulative_weight:
-					selected_cards.append(c)
-					scene_1.card_total_num += 1
-					
-					print(selected_cards)
-					#print("注入select数组")
-					break
-					
+	var index = 0
+	while cards_drawn < num_cards:
+		if not playerDeck.is_empty():
+			# 正常抽牌
+			index = randi_range(0, playerDeck.size() - 1)
+			selected_cards.append(playerDeck[index])
+			playerDeck.remove_at(index)
+			cards_drawn += 1
+		elif not discard_deck.is_empty():
+			# 重置卡组
+			playerDeck = discard_deck.duplicate()
+			playerDeck.shuffle()
+			discard_deck.clear()
+			print("卡组重置并洗牌")
 		else:
-			print("手牌达到上限")
+			# 无牌可抽
+			print("卡组中没有任何卡牌，无法再发牌了")
 			break
+		
 	for c in selected_cards:
+		scene_1.card_total_num += 1
 		await get_tree().create_timer(0.1,1).timeout
 		add_new_hand_card(c, scene_1)
-		add_new_hand_card(c, scene_2)
 	if checkVolley() == true:
 		add_new_hand_card("volley" ,scene_1)
-		
-	
-func get_total_weight(card_dict):
-	var total_weight = 0
-	for weight in card_dict.values():
-		total_weight += weight
-	return total_weight
+
+func get_oppoent_cards():
+	var num_cards = 1
+	var oppoentDeck = get_oppoent_deck()
+	var selected_cards
+	var index = randi_range(0,  oppoentDeck.size() - 1)
+	selected_cards = oppoentDeck[index]
+	for i in range(num_cards):
+		await get_tree().create_timer(0.1,1).timeout
+		add_new_hand_card(selected_cards, scene_2)
 
 #更新数据
 func update_data() -> void:
 	$HandCardPer.value = scene_1.card_total_num
-	$PlayerProperty/PlayerHandCard.text = "手牌：     " + str(scene_1.card_total_num) + " / " + str(MaxHandCard)
+	$PlayerProperty/PlayerHandCard.text = "手牌：     " + str(scene_1.card_total_num) + " / " + str(7)
 	$PlayerProperty/PlayerLife.text = "生命：     " + str(playerLife) + " / " + str(playerMaxLife)
 	$PlayerProperty/PlayerAmmo.text = "弹匣:       " + str(playerAmmo)+ " / " + str(playerMaxAmmo)
 	
 #下一回合
 func next_turn() -> void:
 	var usedCards = $UsedCards.cardDeck.get_children()
+	var oppoentCards = $OppoentCards.cardDeck.get_children()
 	var delCards = 0
 	#结算
-	print(usedCards)
+
 	if not usedCards.is_empty():
-		checkResult(usedCards , usedCards)
+		checkResult(usedCards , oppoentCards)
 	
 	#消除卡牌
 	for c in usedCards:
 		c.cardCurrentState = c.cardState.del
+		
+		#在弃牌堆中加入打出的卡，再从使用栏中将其删掉
+		discard_deck.append(c.cardName)
 		delCards +=1
 	scene_1.card_total_num -= delCards
-
+	
+	#发新的牌
+	for c in oppoentCards:
+		c.cardCurrentState = c.cardState.del
+	get_oppoent_cards()
+	
+	
 #检查伤害结算，返回各式：用户造成的伤害，用户弹药变化，对手造成的伤害，对手弹药变化
 func checkResult(PlayerCards , OppoentCards):
 	var playerDamage = 0
@@ -180,4 +192,13 @@ func checkVolley() -> bool:
 	if playerAmmo == playerMaxAmmo:
 		return true
 	return false
-	
+
+func get_oppoent_deck() -> Array:
+	var oppoentCards = ["shoot", "avoid", "load"]
+	if oppoentAmmo == oppoentMaxAmmo:
+		return ["volley"]
+	if playerAmmo == 0:
+		oppoentCards.filter(func(x): return x != "avoid")
+	if oppoentAmmo == 0:
+		oppoentCards.filter(func(x): return x != "shoot")
+	return oppoentCards
